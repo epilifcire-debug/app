48bf0d339ac637ba58027cc87a96257fd8035b18
Preto T.I <epilif.cire@gmail.com>
2026-02-28 19:25:43 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,26 @@
-const CACHE_NAME="fc-cache-v1";
+const CACHE_NAME = "fc-cache-v2";
 
-const urlsToCache=[
-"./",
-"./index.html",
-"./style.css",
-"./app.js",
-"./logo-512.png",
-"./logo-horizontal.png"
+const BASE_PATH = self.location.pathname.replace("service-worker.js", "");
+
+const urlsToCache = [
+  BASE_PATH,
+  BASE_PATH + "index.html",
+  BASE_PATH + "style.css",
+  BASE_PATH + "app.js",
+  BASE_PATH + "logo-512.png",
+  BASE_PATH + "logo-horizontal.png"
 ];
 
-self.addEventListener("install",event=>{
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache=>cache.addAll(urlsToCache))
-);
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch",event=>{
-event.respondWith(
-caches.match(event.request)
-.then(response=>response||fetch(event.request))
-);
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
